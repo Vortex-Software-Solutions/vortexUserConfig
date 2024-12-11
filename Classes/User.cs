50bd32a487c170701bc6eb33a70cab4bdc118372
DEADM0US3 <ffernando.villafanaalfonseca@gmail.com>
2024-12-11 12:41:39 -0600

    Subiendo metodos para usar desde el user config

--- Classes/User.cs
+++ Classes/User.cs
@@ -8,12 +8,13 @@ namespace vortexUserConfig.Classes;
 public class User
 {
     private string _name;
-    private string _lastName = null;
+    private string _lastName;
     private string _userName; 
     private string _email;
     private string _hashedPassword;
     
-    public User( string name, string lastName, string userName, string email, string password)
+    //Made to create base user that contains the basic information
+    public User(string name, string lastName, string userName, string email, string password)
     {
         _name = name;
         _lastName = lastName;
@@ -22,6 +23,7 @@ public class User
         _hashedPassword = Hash(password);
     }
 
+    //Method to have a password hasher
     private string Hash(string password)
     {
         byte[] inputBytes = Encoding.UTF8.GetBytes(password);
@@ -40,6 +42,7 @@ public class User
         }
     }
 
+    //Compare passwords you give it a string and it gaves you a boolean
     public bool ComparePassword(string password)
     {
         string hasdedPassword = Hash(password); 
@@ -51,6 +54,41 @@ public class User
 
         return false;
     }
+
+    //Change the user name 
+    public bool ChangeUserName(string name, string lastName)
+    {
+        
+        if ( name.Trim() == string.Empty || lastName.Trim() == string.Empty)
+        {
+            return false;
+        }
+        
+        _name = name;
+        _lastName = lastName;
+
+        return true;
+    }
+    
+    //Change the user email
+    public bool ChangeEmail(string email)
+    {
+        if (email.Trim() == string.Empty)
+        {
+            return false;
+        }
+        
+        _email = email;
+        return true;
+    }
+    
+    // Returns the user fullname
+    public string GetFullName()
+    {
+        return $"{_name} {_lastName}";
+    }
+    
+    
     
     
     
